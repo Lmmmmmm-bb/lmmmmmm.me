bb6cedcecbadbdf7d628db172d186a1feba66773
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-01-18 00:00:50 +0800

    feat: vercel analytics support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import 'uno.css';
 import '@unocss/reset/tailwind.css';
+import { inject } from '@vercel/analytics';
 import { ViteSSG } from 'vite-ssg/single-page';
 
 import './index.css';
 import App from './app';
+
+// Vercel Analytics
+inject();
 
 export const createApp = ViteSSG(App);
