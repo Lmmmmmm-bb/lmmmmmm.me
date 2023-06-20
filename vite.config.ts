31bb064ec05a6511dab9b511eaa725626e9a878d
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-06-20 21:42:35 +0800

    refactor: open graph plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,31 @@ import { resolve } from 'path';
 import unocss from 'unocss/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import og from 'vite-plugin-open-graph';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), unocss()],
+  plugins: [
+    vue(),
+    unocss(),
+    og({
+      basic: {
+        type: 'website',
+        title: '_lmmmmmm',
+        url: 'https://lmmmmmm.me',
+        image: 'https://lmmmmmm.me/avatar.jpg',
+        description: '_lmmmmmm, Front-end Developer.',
+      },
+      twitter: {
+        title: '_lmmmmmm',
+        card: 'summary',
+        description: '_lmmmmmm, Front-end Developer.',
+        creator: '@_lmmmmmm',
+        image: 'https://lmmmmmm.me/avatar.jpg',
+        imageAlt: 'lmmmmmm\'s avatar',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
