88151b2435aa4c9fea2b3f3ea7b0c084bb3117e9
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-07-29 14:55:06 +0800

    fix: vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import 'uno.css';
 import '@unocss/reset/tailwind.css';
-import { inject } from '@vercel/analytics';
 import { ViteSSG } from 'vite-ssg/single-page';
 
 import './index.css';
 import App from './app';
-
-// Vercel Analytics
-inject();
 
 export const createApp = ViteSSG(App);
